a32cdd5b0accdf01e189872a2b6c955bfece0040
JaeHyun-Ban <wogus0008@gmail.com>
2020-11-12 17:36:11 +0900

    11/12

--- Gookbe/database/DBSQL/JSP/testweb.sql
+++ Gookbe/database/DBSQL/JSP/testweb.sql
@@ -1,4 +1,5 @@
---글 작성,리스트 기준으로 생성
+--게시판 테이블 생성
+--글번호,작성자,글제목,글내용,등록일
 create table bbs(
     bno NUMBER(10) not null,
     writer VARCHAR2(100) not null,
@@ -6,9 +7,6 @@ create table bbs(
     content VARCHAR2(3000) not null,
     regdate date default sysdate
 )
---bbs
---글번호,작성자,글제목,글내용,등록일
-
 --bno를 pk로 설정, 이름: bbs_pk
 ALTER TABLE bbs ADD CONSTRAINT bbs_pk PRIMARY KEY (bno);
 --bno숫자 자동증가 시퀀스 생성(시작:1, 증가:1 시퀀스 미리 할당x)
@@ -16,7 +14,7 @@ CREATE SEQUENCE bbs_seq START WITH 1 INCREMENT BY 1 NOCACHE;
 commit;
 
 --------------------------------------------------------------
---회원가입 기준으로 생성
+--회원테이블 생성
 create table members(
     id VARCHAR2(50) not null,
     password VARCHAR2(50) not null,
@@ -45,22 +43,15 @@ SELECT * FROM members WHERE id = 'asd1' AND password = 123456;
 ---------------------------------------------------------
 --게시판 test글 넣기
 DECLARE
-
     var1 NUMBER := 1;
-    
 BEGIN
-
     WHILE var1 <= 5
     LOOP
         INSERT INTO bbs(bno, writer, title, content) VALUES(BBS_SEQ.nextval, 'asd1', '123456', 'test'||var1);
-        
-        
-        var1 := var1 + 1;
-        
+        var1 := var1 + 1;       
     end LOOP;
     
     commit;--commit빼먹지 발아주기
-
 END;
 
 select * from bbs order by bno desc;
---
+++ info/required의 메세지를 바꾸는법.md
@@ -0,0 +1,7 @@
+출처
+https://stackoverflow.com/questions/24391078/how-to-change-the-default-message-of-the-required-field-in-the-popover-of-form-c
+
+---
+
+**oninvalid="this.setCustomValidity('문구 작성')"**로 메시지 내용을 변경한 뒤
+**oninput="setCustomValidity('')"**로 입력이 된 후에 메시지가 나오지 않도록 바꿔준다
